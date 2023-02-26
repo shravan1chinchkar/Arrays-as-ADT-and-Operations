a0aec3de1ce73f8dc377e68c0706e1bd4ec993c5
Shravan <shravan009chinchkar100@gmail.com>
2023-02-26 14:35:16 +0530

    again committed The array ADT Program

--- learning1.cpp
+++ learning1.cpp
@@ -6,13 +6,15 @@ class myArray
     int arr_size;
     int used_size;
     int *ptr;
+
     public:
     myArray(myArray *a,int tsize,int usize)
     {
-        a->arr_size=tsize;
+        a->arr_size=tsize;//meaning marks.arr_size
         a->used_size=usize;
-        a->ptr=new int(tsize);//this will create array in heap memory
+        a->ptr=new int(tsize);//this will create array in heap memory i.e dynamically
     }
+
     void insertElements(myArray *a)
     {
         int element;
@@ -20,12 +22,13 @@ class myArray
         for(int i=0;i<a->used_size;i++)
         {
             cin>>element;
-            (a->ptr)[i]=element;
+            (a->ptr)[i]=element;//i.e the array which is stored at address a and the name of arrray is ptr(just for understanding)
         }
     }
+
     void displayArray(myArray *a)
     {
-        cout<<"Marks acchieved by"<<used_size<<" students in maths subject are: "<<endl;
+        cout<<"Marks acchieved by "<<used_size<<" students in maths subject are: "<<endl;
         for (int i = 0; i < a->used_size; i++)
         {
             cout<<(a->ptr)[i]<<endl;
@@ -34,12 +37,16 @@ class myArray
 
 };
 int main(){
+    /*The program will start from here
+    Initially we will enter the total size of an array
+    Then we will enter the size that we want to use
+    */
     int total_size,used_size;
     cout<<"Enter Total size of an array"<<endl;
     cin>>total_size;
     cout<<"Enter how much blocks you want to use from the array"<<endl;
     cin>>used_size;
-
+    //Here the object of Abstract data type is created
     myArray marks(&marks,total_size,used_size);
     marks.insertElements(&marks);
     marks.displayArray(&marks);
