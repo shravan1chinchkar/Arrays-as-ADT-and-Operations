a1564f27b5db17623d2abb320ee14c4dc57340f5
Shravan <shravan009chinchkar100@gmail.com>
2023-03-06 23:17:17 +0530

    completed Stack_Using_LL

--- Stack_Using_LL.cpp
+++ Stack_Using_LL.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
+/*
+Stack using Linked List.
+here very new Node is inserted at the beginning of th LL because here 
+insertion and deletion i.e push and pop operation can be performed in o(1)
+*/
 class Node
 {
     public:
@@ -17,9 +22,10 @@ int isEmpty(Node *top)
         return 0;
     }
 }
-int isFull(Node *newNode)
+int isFull(Node *top)
 {
-    if(newNode==NULL)
+    Node *newNode1=new Node;
+    if(newNode1==NULL)
     {
         return 1;
     }
@@ -28,36 +34,108 @@ int isFull(Node *newNode)
         return 0;
     }
 }
-Node *push(Node *top)
+Node *push(Node *top)//inserting node at beginning.
 {
     int data;
     cout<<"push data in the stack"<<endl;
     cin>>data;
     Node *newNode=new Node;
-    if(isFull(newNode))
+    if(isFull(top))
     {
         cout<<"Stack Overflow"<<endl;
+        return newNode;
     }
     newNode->data=data;
     newNode->next=top;
     top=newNode;
     return top;
 }
+Node *pop(Node *top)
+{
+    if(isEmpty(top))
+    {
+        cout<<"Stack UnderFlow"<<endl;
+        return top;
+    }
+    else
+    {
+        Node *ptr=top;
+        int ans =ptr->data;
+        cout<<"poped :"<<ans<<endl;
+        top=top->next;
+        delete ptr;
+    }
+    return top;
+}
+Node *peek(Node *top)
+{
+    if(isEmpty(top))
+    {
+        cout<<"Stack Underflow";
+        return top;
+    }
+    else{
+        cout<<"Current topmost element is: "<<top->data<<endl;    
+    }
+}
 void display_stack(Node *top)
 {
+    if(top==NULL)
+    {
+        cout<<"Nothing to display"<<endl;
+    }
     while(top!=NULL)
     {
         cout<<"Element: "<<top->data<<endl;
-        cout<<"Address of its successive node: "<<top->next<<endl;
-        cout<<"Address of the current node: "<<top<<endl;
+        // cout<<"Address of its successive node: "<<top->next<<endl;
+        // cout<<"Address of the current node: "<<top<<endl;
         top=top->next;
     }
 }
 int main(){
     Node *top=NULL;
-    top=push(top);
-    display_stack(top);
-    top=push(top);
-    display_stack(top);
+    int ch;
+    while(true)
+    {
+        cout<<endl;
+        cout<<"**************************MENU***********************"<<endl;
+        cout<<"1:To check stack is empty or not "<<endl;
+        cout<<"2:To check stack is full or not "<<endl;
+        cout<<"3:push element "<<endl;
+        cout<<"4:pop element "<<endl;
+        cout<<"5:To see which is the top most element "<<endl;
+        cout<<"6:Display stack "<<endl;
+        cout<<"7:Exit"<<endl;
+        cout<<"************************MENU_ENDS*******************"<<endl;
+        cout<<"Enter your choice: "<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:
+                cout<<isEmpty(top);
+                break;
+            case 2:
+                cout<<isFull(top);
+                break;
+            case 3:
+                top=push(top);
+                break;
+            case 4:
+                top=pop(top);
+                break;
+            case 5:
+                top=peek(top);
+                break;
+            case 6:
+                display_stack(top);
+                break;
+            case 7:
+                exit(0);
+                break;
+            default:
+                cout<<"Please enter correct choice"<<endl;
+                break;
+        }
+    }
     return 0;
 }
