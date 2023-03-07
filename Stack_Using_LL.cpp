bf91aa11e927d2ee7780dfefa1143d9f17468f92
Shravan <shravan009chinchkar100@gmail.com>
2023-03-07 11:39:25 +0530

    Final commit of stack_Using_LL

--- Stack_Using_LL.cpp
+++ Stack_Using_LL.cpp
@@ -22,9 +22,9 @@ int isEmpty(Node *top)
         return 0;
     }
 }
-int isFull(Node *top)
+int isFull(Node *top)//if we cant allocate memory dynamically then it is full
 {
-    Node *newNode1=new Node;
+   Node *newNode1=new Node;
     if(newNode1==NULL)
     {
         return 1;
@@ -36,15 +36,15 @@ int isFull(Node *top)
 }
 Node *push(Node *top)//inserting node at beginning.
 {
+    if(isFull(top))
+    {
+        cout<<"Stack Overflow"<<endl;
+        return top;
+    }
     int data;
     cout<<"push data in the stack"<<endl;
     cin>>data;
     Node *newNode=new Node;
-    if(isFull(top))
-    {
-        cout<<"Stack Overflow"<<endl;
-        return newNode;
-    }
     newNode->data=data;
     newNode->next=top;
     top=newNode;
@@ -67,7 +67,7 @@ Node *pop(Node *top)
     }
     return top;
 }
-Node *peek(Node *top)
+Node *peek(Node *top)//return the top most element
 {
     if(isEmpty(top))
     {
@@ -78,7 +78,44 @@ Node *peek(Node *top)
         cout<<"Current topmost element is: "<<top->data<<endl;    
     }
 }
-void display_stack(Node *top)
+void return_element(Node *top)
+{
+    Node *ptr=top;
+    int pos;
+    cout<<"Enter position of element you want :"<<endl;
+    cin>>pos;
+    for(int i=1;(i<pos && ptr!=NULL);i++)
+    {
+        ptr=ptr->next;
+    }
+    if(ptr==NULL)
+    {
+        cout<<"Invalid index"<<endl;
+        return;
+    }
+    cout<<"Element at position "<<pos<<" is :"<<ptr->data<<endl;
+}
+Node  *bottom(Node *top)
+{
+    if(isEmpty(top))
+    {
+        cout<<"Stack UnderFlow"<<endl;
+        return top;
+    }
+    Node *ptr=top;
+    if(ptr->next==NULL)
+    {
+      cout<<"Bottommost Element is: "<<ptr->data;
+      return top;  
+    }
+    while(ptr->next!=NULL)//if there are more than 1 nodes in a Linked List
+    {
+        ptr=ptr->next;
+    }
+    cout<<"Bottommost Element is: "<<ptr->data;
+    return top;
+}
+void display_stack(Node *top)//display 
 {
     if(top==NULL)
     {
@@ -105,7 +142,9 @@ int main(){
         cout<<"4:pop element "<<endl;
         cout<<"5:To see which is the top most element "<<endl;
         cout<<"6:Display stack "<<endl;
-        cout<<"7:Exit"<<endl;
+        cout<<"7:Bottom most element"<<endl;
+        cout<<"8:Return element of specific position"<<endl;
+        cout<<"9:Exit"<<endl;
         cout<<"************************MENU_ENDS*******************"<<endl;
         cout<<"Enter your choice: "<<endl;
         cin>>ch;
@@ -130,8 +169,13 @@ int main(){
                 display_stack(top);
                 break;
             case 7:
-                exit(0);
+                top=bottom(top);
                 break;
+            case 8:
+                return_element(top);
+                break;
+            case 9:
+                exit(0);
             default:
                 cout<<"Please enter correct choice"<<endl;
                 break;
